2cf6e0b642a3224bb1cb3abba69a6de05f4b835c
johnsturgeon <john.sturgeon@me.com>
2021-03-13 12:13:16 -0800

    Ready for first test of package version 0.1.0 (#1)
    - Updated .gitignore to keep PyCharm project files and mac .DS_Store out
    - Split out the requirements between 'dev' and production
    Turned it into an official 'package' for distribution to PyPi
    - 
    * Ready for first test of package version 0.1.0
    
    * Published and tested on PyPi Test server
    
    It's now working on PyPi test server, there are still odds and ends to do to get it to a version 0.0.1 that actually gets published to PyPi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='apex-legends-api',
+    version='0.1.9',
+    packages=['apex_legends_api'],
+    python_requires='>=3.6.*',
+    url='https://github.com/johnsturgeon/apex-legends-api',
+    license='MIT',
+    author='John Sturgeon',
+    author_email='john.sturgeon@me.com',
+    install_requires=['requests >= 2.5.4.1'],
+    description='Python wrapper for https://apexlegendsapi.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
