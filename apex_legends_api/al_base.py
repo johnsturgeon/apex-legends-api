d6f7987fd276e5eba5c9845fc36335a09f3a865a
John Sturgeon <john.sturgeon@me.com>
2021-03-20 19:14:03 -0700

    Added better descriptions to the classes

--- apex_legends_api/al_base.py
+++ apex_legends_api/al_base.py
@@ -4,6 +4,28 @@ contains some of the base / utility classes and Enums
 """
 from enum import Enum
 
+DESCRIPTION_DEPTH: int = 0
+
+
+def description(obj) -> str:
+    """ utility method for returning a string describing the object given """
+    global DESCRIPTION_DEPTH  # noqa W0603
+    DESCRIPTION_DEPTH += 1
+    tabs = ""
+    for _ in range(1, DESCRIPTION_DEPTH):
+        tabs += "\t"
+    # return_str: str = f"\n{tabs}" + type(obj).__name__ + "\n"
+    return_str: str = "\n"
+    for attribute_name, attribute_value in obj.__dict__.items():
+        if isinstance(attribute_value, list):
+            return_str += f"{tabs}{attribute_name}:"
+            for inner_attribute in attribute_value:
+                return_str += f"{tabs}{inner_attribute}"
+        else:
+            return_str += f"{tabs}{attribute_name}: {attribute_value}\n"
+    DESCRIPTION_DEPTH -= 1
+    return return_str
+
 
 class ALPlatform(Enum):
     """ Three platforms available """
--- apex_legends_api/al_domain.py
+++ apex_legends_api/al_domain.py
@@ -2,7 +2,7 @@
 Player class for the Apex Legends API Python package
 """
 import arrow
-from .al_base import ALPlatform  # noqa E0402
+from .al_base import ALPlatform, description  # noqa E0402
 
 
 # pylint: disable=too-few-public-methods
@@ -18,6 +18,9 @@ class GlobalInfo:
             self.is_active: bool = bool(bans_dict.get('isActive'))
             self.seconds_remaining: int = bans_dict.get('remainingSeconds')
 
+        def __str__(self):
+            return description(self)
+
     class Rank:
         """ data structure for player rank information """
         def __init__(self, rank_dict: dict):
@@ -27,6 +30,9 @@ class GlobalInfo:
             self.ladder_pos_platform: int = rank_dict.get('ladderPosPlatform')
             self.image_url: str = rank_dict.get('rankImg')
             self.season: str = rank_dict.get('rankedSeason')
+
+        def __str__(self):
+            return description(self)
 
         @property
         def rank_division_roman(self) -> str:
@@ -52,10 +58,13 @@ class GlobalInfo:
         self.bans: GlobalInfo.Bans = GlobalInfo.Bans(bans_dict=global_dict.get('bans'))
         self.rank: GlobalInfo.Rank = GlobalInfo.Rank(rank_dict=global_dict.get('rank'))
 
+    def __str__(self):
+        return description(self)
+
 
 class RealtimeInfo:
     """ a data structure for the player's real time information """
-    def __init__(self, realtime_dict):
+    def __init__(self, realtime_dict: dict):
         self.lobby_state: str = realtime_dict.get('lobbyState')
         self.is_online: bool = bool(realtime_dict.get('isOnline'))
         self.is_in_game: bool = bool(realtime_dict.get('isInGame'))
@@ -64,12 +73,79 @@ class RealtimeInfo:
         self.selected_legend: str = realtime_dict.get('selectedLegend')
 
     def __str__(self):
-        return f"Lobby State: {self.lobby_state}\n"\
-               f"Is Online: {self.is_online}\n" \
-               f"Is In Game: {self.is_in_game}\n" \
-               f"Can Join: {self.can_join}\n" \
-               f"Party Full: {self.party_full}\n" \
-               f"Selected Legend: {self.selected_legend}\n"
+        return description(self)
+
+
+class DataTrackers:
+    """ data structure for badges """
+    def __init__(self, data_trackers_dict: dict):
+        self.name = data_trackers_dict.get('name')
+        self.value = data_trackers_dict.get('value')
+        self.key = data_trackers_dict.get('key')
+
+    def __str__(self):
+        return description(self)
+
+
+class ImgAssets:
+    """ data structure for image assets """
+    def __init__(self, image_asset_dict: dict):
+        self.icon = image_asset_dict.get('icon')
+        self.banner = image_asset_dict.get('banner')
+
+    def __str__(self):
+        return description(self)
+
+
+class SelectedLegend:
+    """ a data structure for the player's selected legend """
+    class GameInfo:
+        """ data structure for game information for selected legend """
+        class Badge:
+            """ data structure for badges """
+            def __init__(self, badge_dict: dict):
+                self.name = badge_dict.get('name')
+                self.value = badge_dict.get('value')
+
+            def __str__(self):
+                return description(self)
+
+        def __init__(self, game_info_dict: dict):
+            self.skin = game_info_dict.get('skin')
+            self.frame = game_info_dict.get('frame')
+            self.pose = game_info_dict.get('pose')
+            self.intro = game_info_dict.get('intro')
+            self.badges = list()
+            for badge in game_info_dict.get('badges'):
+                self.badges.append(SelectedLegend.GameInfo.Badge(badge))
+
+        def __str__(self):
+            return description(self)
+
+    def __init__(self, selected_legend_dict: dict):
+        self.legend_name = selected_legend_dict.get('LegendName')
+        self.data_trackers = list()
+        for data_tracker in selected_legend_dict.get('data'):
+            self.data_trackers.append(DataTrackers(data_tracker))
+        self.game_info = SelectedLegend.GameInfo(selected_legend_dict.get('gameInfo'))
+        self.img_assets = ImgAssets(selected_legend_dict.get('ImgAssets'))
+
+    def __str__(self):
+        return description(self)
+
+
+class Legend:
+    """ data structure for NON selected legend """
+    def __init__(self, legend_name: str, legend_dict: dict):
+        self.name = legend_name
+        self.data_trackers = list()
+        if legend_dict.get('data'):
+            for data_tracker in legend_dict.get('data'):
+                self.data_trackers.append(DataTrackers(data_tracker))
+        self.img_assets = ImgAssets(legend_dict.get('ImgAssets'))
+
+    def __str__(self):
+        return description(self)
 
 
 class ALPlayer:
@@ -87,3 +163,12 @@ class ALPlayer:
         self.realtime_info: RealtimeInfo = RealtimeInfo(realtime_dict=basic_player_stats_data['realtime'])
         self.matches_tracked: bool = False
         self.timestamp_last_checked: int = arrow.utcnow().int_timestamp
+        self.selected_legend: SelectedLegend = SelectedLegend(
+            selected_legend_dict=basic_player_stats_data['legends']['selected']
+        )
+        self.all_legends: list[Legend] = list()
+        for legend_name, legend_dict in basic_player_stats_data['legends']['all'].items():
+            self.all_legends.append(Legend(legend_name=legend_name, legend_dict=legend_dict))
+
+    def __str__(self):
+        return description(self)
