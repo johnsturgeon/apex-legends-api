e73cc181e1655110ea11afa722f32101dddbd21a
John Sturgeon <john.sturgeon@me.com>
2021-03-28 09:31:11 -0700

    Resolves #27 (running unit tests locally failed for me)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 """ configure test fixtures """
+import os
 import json
 import pytest
 import requests_mock
+
+
+def get_full_filepath(test_filename):
+    """ return a fully qualified file location for the test file"""
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    return_filepath = os.path.abspath(file_path + "/responses/" + test_filename)
+    return return_filepath
 
 
 # pylint: disable=missing-function-docstring
@@ -13,23 +21,23 @@ def mock():
 
 @pytest.fixture()
 def basic_player_stats_response():
-    with open('tests/responses/basic_player_stats_response.json') as json_file:
+    with open(get_full_filepath('basic_player_stats_response.json')) as json_file:
         yield json.load(json_file)
 
 
 @pytest.fixture()
 def match_history_get_response():
-    with open('tests/responses/match_history_get_response.json') as json_file:
+    with open(get_full_filepath('match_history_get_response.json')) as json_file:
         yield json.load(json_file)
 
 
 @pytest.fixture()
 def match_history_info_response():
-    with open('tests/responses/match_history_info_response.json') as json_file:
+    with open(get_full_filepath('match_history_info_response.json')) as json_file:
         yield json.load(json_file)
 
 
 @pytest.fixture()
 def player_origin_response():
-    with open('tests/responses/player_origin_response.json') as json_file:
+    with open(get_full_filepath('player_origin_response.json')) as json_file:
         yield json.load(json_file)
